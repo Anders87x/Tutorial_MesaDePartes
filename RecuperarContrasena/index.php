25aaeeb4523de13a10934319635651512026285a
Anderson <davis_anderson_87@hotmail.com>
2020-06-05 23:41:52 -0500

    05/06/2020 Subiendo tutorial a Github

---
+++ RecuperarContrasena/index.php
@@ -0,0 +1,87 @@
+<html lang="en" class="no-focus">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+
+        <title>Recuperar Contraseña | Mesa de Partes</title>
+
+        <meta name="description" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta name="author" content="pixelcave">
+        <meta name="robots" content="noindex, nofollow">
+        <meta property="og:title" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework">
+        <meta property="og:site_name" content="Codebase">
+        <meta property="og:description" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="">
+        <meta property="og:image" content="">
+        <link rel="shortcut icon" href="../public/assets/img/favicons/favicon.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="../public/assets/img/favicons/favicon-192x192.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="../public/assets/img/favicons/apple-touch-icon-180x180.png">
+        <link rel="stylesheet" id="css-main" href="../public/assets/css/codebase.min.css">
+    </head>
+    <body>
+        <div id="page-container" class="main-content-boxed">
+            <main id="main-container">
+                <div class="bg-image" style="background-image: url('../public/assets/img/photos/photo34@2x.jpg');">
+                    <div class="row mx-0 bg-black-op">
+                        <div class="hero-static col-md-6 col-xl-8 d-none d-md-flex align-items-md-end">
+                            <div class="p-30 invisible" data-toggle="appear">
+                                <p class="font-size-h3 font-w600 text-white">
+                                    Nombre Empresa
+                                </p>
+                                <p class="font-italic text-white-op">
+                                    Copyright &copy; <span class="js-year-copy">2017</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="hero-static col-md-6 col-xl-4 d-flex align-items-center bg-white invisible" data-toggle="appear" data-class="animated fadeInRight">
+                            <div class="content content-full">
+                                <!-- Header -->
+                                <div class="px-30 py-10">
+                                    <a class="link-effect font-w700" href="index.html">
+                                        <i class="si si-fire"></i>
+                                        <span class="font-size-xl text-primary-dark">Mesa de </span><span class="font-size-xl">Partes</span>
+                                    </a>
+                                    <h2 class="h5 font-w400 text-muted mb-0">Recuperar Contraseña</h2>
+                                </div>
+                                <form class="js-validation-signin px-30" action="be_pages_auth_all.html" method="post">
+                                    <div class="form-group row">
+                                        <div class="col-12">
+                                            <div class="form-material floating">
+                                                <input type="text" class="form-control" id="login-username" name="login-username">
+                                                <label for="login-username">Correo Electronico</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-sm btn-hero btn-alt-primary">
+                                            <i class="si si-login mr-10"></i> Recuperar
+                                        </button>
+                                        <div class="mt-30">
+                                            <a class="link-effect text-muted mr-10 mb-5 d-inline-block" href="../">
+                                                <i class="fa fa-plus mr-5"></i> Acceso
+                                            </a>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+
+        <script src="../public/assets/js/core/jquery.min.js"></script>
+        <script src="../public/assets/js/core/popper.min.js"></script>
+        <script src="../public/assets/js/core/bootstrap.min.js"></script>
+        <script src="../public/assets/js/core/jquery.slimscroll.min.js"></script>
+        <script src="../public/assets/js/core/jquery.scrollLock.min.js"></script>
+        <script src="../public/assets/js/core/jquery.appear.min.js"></script>
+        <script src="../public/assets/js/core/jquery.countTo.min.js"></script>
+        <script src="../public/assets/js/core/js.cookie.min.js"></script>
+        <script src="../public/assets/js/codebase.js"></script>
+        <script src="../public/assets/js/plugins/jquery-validation/jquery.validate.min.js"></script>
+        <script src="../public/assets/js/pages/op_auth_signin.js"></script>
+    </body>
+</html>
---
+++ Registrarse/index.php
@@ -0,0 +1,163 @@
+<html lang="en" class="no-focus">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Registrase | Mesa de Partes</title>
+        <meta name="description" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta name="author" content="pixelcave">
+        <meta name="robots" content="noindex, nofollow">
+        <meta property="og:title" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework">
+        <meta property="og:site_name" content="Codebase">
+        <meta property="og:description" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="">
+        <meta property="og:image" content="">
+        <link rel="shortcut icon" href="../public/assets/img/favicons/favicon.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="../public/assets/img/favicons/favicon-192x192.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="../public/assets/img/favicons/apple-touch-icon-180x180.png">
+        <link rel="stylesheet" id="css-main" href="../public/assets/css/codebase.min.css">
+    </head>
+    <body>
+ 
+        <div id="page-container" class="main-content-boxed">
+            <!-- Main Container -->
+            <main id="main-container">
+                <!-- Page Content -->
+                <div class="bg-image" style="background-image: url('../public/assets/img/photos/photo34@2x.jpg');">
+                    <div class="row mx-0 bg-earth-op">
+                        <div class="hero-static col-md-6 col-xl-8 d-none d-md-flex align-items-md-end">
+                            <div class="p-30 invisible" data-toggle="appear">
+                                <p class="font-size-h3 font-w600 text-white mb-5">
+                                   Empresa
+                                </p>
+                                <p class="font-size-h5 text-white">
+                                    <i class="fa fa-angles-right"></i> www.Paginaweb.com
+                                </p>
+                                <p class="font-italic text-white-op">
+                                    Copyright &copy; <span class="js-year-copy">2017</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="hero-static col-md-6 col-xl-4 d-flex align-items-center bg-white">
+                            <div class="content content-full">
+                                <!-- Header -->
+                                <div class="px-30 py-10">
+                                    <a class="link-effect font-w700" href="index.html">
+                                        <i class="si si-fire"></i>
+                                        <span class="font-size-xl text-primary-dark">Mesa de</span><span class="font-size-xl">Partes</span>
+                                    </a>
+                                    <h1 class="h3 font-w700 mt-30 mb-10">Crear Cuenta</h1>
+                                    <h2 class="h5 font-w400 text-muted mb-0">Por favor agrege los detalles</h2>
+                                </div>
+                                <form class="js-validation-signup px-30" action="be_pages_auth_all.html" method="post">
+                                    <div class="form-group row">
+                                        <div class="col-12">
+                                            <div class="form-material floating">
+                                                <input type="text" class="form-control" id="signup-username" name="signup-username">
+                                                <label for="signup-username">Nombre y Apellido</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-12">
+                                            <div class="form-material floating">
+                                                <input type="email" class="form-control" id="signup-email" name="signup-email">
+                                                <label for="signup-email">Email</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-12">
+                                            <div class="form-material floating">
+                                                <input type="password" class="form-control" id="signup-password" name="signup-password">
+                                                <label for="signup-password">Contraseña</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-12">
+                                            <div class="form-material floating">
+                                                <input type="password" class="form-control" id="signup-password-confirm" name="signup-password-confirm">
+                                                <label for="signup-password-confirm">Confirmar Contraseña</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-12">
+                                            <label class="custom-control custom-checkbox">
+                                                <input type="checkbox" class="custom-control-input" id="signup-terms" name="signup-terms">
+                                                <span class="custom-control-indicator"></span>
+                                                <span class="custom-control-description">I agree to Terms &amp; Conditions</span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-sm btn-hero btn-alt-success">
+                                            <i class="fa fa-plus mr-10"></i> Crear Cuenta
+                                        </button>
+                                        <div class="mt-30">
+                                            <a class="link-effect text-muted mr-10 mb-5 d-inline-block" href="#" data-toggle="modal" data-target="#modal-terms">
+                                                <i class="fa fa-book text-muted mr-5"></i> Leer Terminos y condiciones
+                                            </a>
+                                            <a class="link-effect text-muted mr-10 mb-5 d-inline-block" href="../">
+                                                <i class="fa fa-user text-muted mr-5"></i> Acceso
+                                            </a>
+                                        </div>
+                                    </div>
+                                </form>
+                                <!-- END Sign Up Form -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- END Page Content -->
+
+            </main>
+            <!-- END Main Container -->
+        </div>
+        <!-- END Page Container -->
+
+        <!-- Terms Modal -->
+        <div class="modal fade" id="modal-terms" tabindex="-1" role="dialog" aria-labelledby="modal-terms" aria-hidden="true">
+            <div class="modal-dialog modal-lg modal-dialog-slidedown" role="document">
+                <div class="modal-content">
+                    <div class="block block-themed block-transparent mb-0">
+                        <div class="block-header bg-primary-dark">
+                            <h3 class="block-title">Terms &amp; Conditions</h3>
+                            <div class="block-options">
+                                <button type="button" class="btn-block-option" data-dismiss="modal" aria-label="Close">
+                                    <i class="si si-close"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="block-content">
+                            <p>Potenti elit lectus augue eget iaculis vitae etiam, ullamcorper etiam bibendum ad feugiat magna accumsan dolor, nibh molestie cras hac ac ad massa, fusce ante convallis ante urna molestie vulputate bibendum tempus ante justo arcu erat accumsan adipiscing risus, libero condimentum venenatis sit nisl nisi ultricies sed, fames aliquet consectetur consequat nostra molestie neque nullam scelerisque neque commodo turpis quisque etiam egestas vulputate massa, curabitur tellus massa venenatis congue dolor enim integer luctus, nisi suscipit gravida fames quis vulputate nisi viverra luctus id leo dictum lorem, inceptos nibh orci.</p>
+                            <p>Potenti elit lectus augue eget iaculis vitae etiam, ullamcorper etiam bibendum ad feugiat magna accumsan dolor, nibh molestie cras hac ac ad massa, fusce ante convallis ante urna molestie vulputate bibendum tempus ante justo arcu erat accumsan adipiscing risus, libero condimentum venenatis sit nisl nisi ultricies sed, fames aliquet consectetur consequat nostra molestie neque nullam scelerisque neque commodo turpis quisque etiam egestas vulputate massa, curabitur tellus massa venenatis congue dolor enim integer luctus, nisi suscipit gravida fames quis vulputate nisi viverra luctus id leo dictum lorem, inceptos nibh orci.</p>
+                            <p>Potenti elit lectus augue eget iaculis vitae etiam, ullamcorper etiam bibendum ad feugiat magna accumsan dolor, nibh molestie cras hac ac ad massa, fusce ante convallis ante urna molestie vulputate bibendum tempus ante justo arcu erat accumsan adipiscing risus, libero condimentum venenatis sit nisl nisi ultricies sed, fames aliquet consectetur consequat nostra molestie neque nullam scelerisque neque commodo turpis quisque etiam egestas vulputate massa, curabitur tellus massa venenatis congue dolor enim integer luctus, nisi suscipit gravida fames quis vulputate nisi viverra luctus id leo dictum lorem, inceptos nibh orci.</p>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-alt-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-alt-success" data-dismiss="modal">
+                            <i class="fa fa-check"></i> Perfect
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="../public/assets/js/core/jquery.min.js"></script>
+        <script src="../public/assets/js/core/popper.min.js"></script>
+        <script src="../public/assets/js/core/bootstrap.min.js"></script>
+        <script src="../public/assets/js/core/jquery.slimscroll.min.js"></script>
+        <script src="../public/assets/js/core/jquery.scrollLock.min.js"></script>
+        <script src="../public/assets/js/core/jquery.appear.min.js"></script>
+        <script src="../public/assets/js/core/jquery.countTo.min.js"></script>
+        <script src="../public/assets/js/core/js.cookie.min.js"></script>
+        <script src="../public/assets/js/codebase.js"></script>
+
+        <script src="../public/assets/js/plugins/jquery-validation/jquery.validate.min.js"></script>
+
+        <script src="../public/assets/js/pages/op_auth_signup.js"></script>
+    </body>
+</html>

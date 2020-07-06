889b97e9943f7e651380c66e9df7ce97902119fb
Anderson <davis_anderson_87@hotmail.com>
2020-07-06 00:35:38 -0500

    06/07/2020 Parte 13

---
+++ controller/email.php
@@ -0,0 +1,24 @@
+<?php
+    require_once("../config/conexion.php");
+    require_once("../models/Email.php");
+    $email = new Email();
+
+    require_once("../models/Usuario.php");
+    $usuario = new Usuario();
+
+    switch($_GET["op"]){
+
+        case "send_recuperar":
+            $email->recuperar($_POST["usu_correo"]);
+        break;
+
+        case "send_nuevo":
+            $email->nuevo($_POST["usu_correo"]);
+        break;
+
+        case "send_solicitud":
+            $email->solicitud($_POST["part_id"],$_POST["usu_nom"],$_POST["usu_ape"]);
+        break;
+ 
+    }
+?>
---
+++ models/Email.php
@@ -0,0 +1,96 @@
+<?php
+    require ('class.phpmailer.php');
+    include("class.smtp.php");
+
+    require_once("../config/conexion.php");
+    require_once("../Usuario.php");
+
+    class Email extends PHPMailer{
+        
+        public function recuperar($usu_correo){
+            $usuario = new Usuario();
+            $datos = $usuario->get_correo_usuario($usu_correo);
+            foreach ($datos as $row) {
+                $nom = $row["usu_nom"].''.$row["usu_ape"];
+                $pass = $row["usu_pass"];
+            }
+
+            $this->IsSMTP();
+            $this->Host = 'smtp.office365.com';
+            $this->Port = 587;
+            $this->SMTPAuth = true;
+            $this->Username = $this->tu_email="alerta.auto@transtotalperu.com";
+            $this->Password = $this->tu_password="tucontraseña";
+            $this->SMTPSecure = 'tls';
+            $this->From = $this->tu_email;
+            $this->FromName = $this->tu_nombre="Recuperar Contraseña";
+            $this->CharSet='UTF8';
+            $this->addAddress("davis_anderson_87@hotmail.com");
+            $this->WordWrap = 50;
+            $this->IsHTML(true);
+            $this->Subject = "Recuperar Contraseña";
+                $cuerpo = file_get_contents('../public/recuperar.html');
+                $cuerpo = str_replace('lblnomx',$nom,$cuerpo);
+                $cuerpo = str_replace('lblpassx',$pass,$cuerpo);
+            $this->Body = $cuerpo;
+            $this->IsHTML(true);
+            $this->AltBody = strip_tags("Recuperar Contraseña");
+            return $this->Send();
+        }
+
+        public function nuevo($usu_correo){
+            $usuario = new Usuario();
+            $datos = $usuario->get_correo_usuario($usu_correo);
+            foreach ($datos as $row) {
+                $nom = $row["usu_nom"].''.$row["usu_ape"];
+                $pass = $row["usu_pass"];
+            }
+
+            $this->IsSMTP();
+            $this->Host = 'smtp.office365.com';
+            $this->Port = 587;
+            $this->SMTPAuth = true;
+            $this->Username = $this->tu_email="alerta.auto@transtotalperu.com";
+            $this->Password = $this->tu_password="tucontraseña";
+            $this->SMTPSecure = 'tls';
+            $this->From = $this->tu_email;
+            $this->FromName = $this->tu_nombre="Registro Correcto";
+            $this->CharSet='UTF8';
+            $this->addAddress("davis_anderson_87@hotmail.com");
+            $this->WordWrap = 50;
+            $this->IsHTML(true);
+            $this->Subject = "Registro Correcto";
+                $cuerpo = file_get_contents('../public/nuevo.html');
+                $cuerpo = str_replace('lblnomx',$nom,$cuerpo);
+            $this->Body = $cuerpo;
+            $this->IsHTML(true);
+            $this->AltBody = strip_tags("Registro Correcto");
+            return $this->Send();
+        }
+
+        public function solicitud($part_id,$usu_nom,$usu_ape){
+            $this->IsSMTP();
+            $this->Host = 'smtp.office365.com';
+            $this->Port = 587;
+            $this->SMTPAuth = true;
+            $this->Username = $this->tu_email="alerta.auto@transtotalperu.com";
+            $this->Password = $this->tu_password="tucontraseña";
+            $this->SMTPSecure = 'tls';
+            $this->From = $this->tu_email;
+            $this->FromName = $this->tu_nombre="Nueva Solicitud";
+            $this->CharSet='UTF8';
+            $this->addAddress("davis_anderson_87@hotmail.com");
+            $this->WordWrap = 50;
+            $this->IsHTML(true);
+            $this->Subject = "Nueva Solicitud";
+                $cuerpo = file_get_contents('../public/solicitud.html');
+                $cuerpo = str_replace('lblnomx',$usu_nom.''.$usu_ape,$cuerpo);
+                $cuerpo = str_replace('lblnumx',$part_id,$cuerpo);
+            $this->Body = $cuerpo;
+            $this->IsHTML(true);
+            $this->AltBody = strip_tags("Nueva Solicitud");
+            return $this->Send();
+        }
+
+    }
+?>
